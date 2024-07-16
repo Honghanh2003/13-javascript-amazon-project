57867ca4cec8ce2feb3378bf6ed50c93d54b2e91
honghanh <hhong2047@gmail.com>
2024-07-16 21:48:01 +0700

    Learn more details about this

--- data/products.js
+++ data/products.js
@@ -46,6 +46,36 @@ class Clothing extends Product {
     `;
   }
 }
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+
+console.log(this);
+
+const object2 = {
+  a:2,
+  b: this.a
+};
+*/
+
+/*
+function logThis(){
+  console.log(this);
+}
+
+logThis();
+logThis.call('Hello');
+
+this
+const objetct3 = {
+  method: ()=>{
+    console.log(this);
+  }
+};
+
+objetct3.method();
+*/
 
 export const products = [
   {
